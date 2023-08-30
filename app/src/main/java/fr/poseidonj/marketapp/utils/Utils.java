fe8afb9e21f7260b7257a33bb8733ea6456445ab
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-08-30 10:50:20 +0200

    start intent with bundle

--- app/src/main/java/fr/poseidonj/marketapp/utils/Utils.java
+++ app/src/main/java/fr/poseidonj/marketapp/utils/Utils.java
@@ -2,9 +2,20 @@ package fr.poseidonj.marketapp.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+
+import java.io.Serializable;
 
 public class Utils {
-    public static Intent startIntent(Context context, Class<?> activityTarget){
+    public static Intent startIntent(Context context, Class<?> activityTarget) {
         return new Intent(context, activityTarget);
+    }
+
+    public static Intent startIntentWithBundle(Context context, Class<?> activityTarget, Serializable object) {
+        Intent intent = startIntent(context, activityTarget);
+        Bundle bundle=new Bundle();
+        bundle.putSerializable("data", object);
+        intent.putExtras(bundle);
+        return intent;
     }
 }
